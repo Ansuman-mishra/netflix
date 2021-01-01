071018c09f541f91197f579b150abda59afddefa
Ansuman-mishra <66157597+Ansuman-mishra@users.noreply.github.com>
2021-01-01 16:12:17 +0530

    commited

---
+++ src/SData.jsx
@@ -0,0 +1,39 @@
+const SData = [
+  {
+    imgscr:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiAci3ZPn4Pu1BEqPsObJfMb_G02aJ_tzPxaGEmvEuAZ32LiwK",
+    title: "A Netflix Orignal Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    imgscr:
+      "https://images-na.ssl-images-amazon.com/images/I/81QDU13hAAL._AC_SL1200_.jpg",
+    title: "A Netflix Orignal Series",
+    sname: "Stranger Things",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+    imgscr:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQmDTJVZoOrJAJUqsTLOaXEARJ-dv-VfTY1wekUuHy9Ps1PASUi",
+    title: "A Netflix Orignal Series",
+    sname: "Marvel's The Punisher",
+    link: "https://www.netflix.com/in/title/80117498",
+  },
+  {
+    imgscr:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTXfHwGsSQvrjv6r5ccVQ_oZsj85b5ezGaTyuIdBpyFllhLk5Ga",
+    title: "A Netflix Orignal Series",
+    sname: "The BlackList",
+    link: "https://www.netflix.com/in/title/70281312",
+  },
+  {
+    imgscr:
+      "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+    title: "A Netflix Orignal Series",
+    sname: "Extra Curricular",
+    link: "https://www.netflix.com/in/title/80990668",
+  },
+];
+
+export default SData;
